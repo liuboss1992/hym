0b687ad647d4ea2b333de110c5dd1a6176b3972c
刘传彬 <liuboss1992@outlook.com>
2019-03-25 01:59:43 +0800

    add code

---
+++ README.md
@@ -0,0 +1,47 @@
+# 类脑平台资源检索邮件通知
+
+## main.py
+主程序
+
+执行
+
+```
+python3 main.py --cpu8 --gpu4 --mem 16384 --sn lcb592 --pw "不告诉你" --ra 774054270@qq.com
+```
+
+当有资源满足你的需求时，你会在终端得到
+
+```shell
+Found!
+```
+
+同时收到邮件
+
+![image](./img/mail.png)
+
+## mailapp.py
+邮件通知程序
+
+```shell
+from mailapp import ustcmail
+ustcmail("lcb592","不告诉你密码","774054270@qq.com","正文")
+```
+
+## monitor.py
+资源监视程序
+
+```
+from monitor import monitor1080
+i, text = monitor1080(cpu需求，gpu需求，内存需求)
+```
+返回值 i 为是否找到的标志，text为资源信息文本。此文本即邮件通知程序的正文。
+
+
+---
+## 修改记录
+
+    20181121
+        根据类脑集群提供的隔离资源，在代码中添加了爬虫隔离资源并屏蔽隔离资源。
+---
+
+如有其他疑问联系本人：liuchuanbin1992@gmail.com
---
+++ main.py
@@ -0,0 +1,49 @@
+import time
+import datetime
+from mailapp import ustcmail
+from monitor import monitordebug, monitor1080, monitorv100, monitorxp
+import smtplib
+import argparse
+
+parser = argparse.ArgumentParser(description='Mail Sender')
+parser.add_argument('--cpu', default='4', type=int, help='CPU Number')
+parser.add_argument('--gpu', default='1', type=int, help='GPU Number')
+parser.add_argument('--mem', default='8192', type=int, help='Memory Size')
+parser.add_argument('--sn', default='lcb592', type=str, help='name of sender account, must be ustc mail')
+parser.add_argument('--pw', default='123456', type=str, help='password of your account')
+parser.add_argument('--ra', default='774054270@qq.com', type=str, help='address of the receiver')
+parser.add_argument('--type', default='1', type=int, help='GPU Type: 0->debug, 1->1080ti, 2->v100, 3->xp')
+args = parser.parse_args()
+
+def main():
+
+    i = 0
+    while (1-i):
+        if args.type == 0:
+            i,text = monitordebug(args.cpu,args.gpu,args.mem)
+        elif args.type == 1:
+            i,text = monitor1080(args.cpu,args.gpu,args.mem)
+        elif args.type == 2:
+            i,text = monitorv100(args.cpu,args.gpu,args.mem)
+        elif args.type == 3:
+            i,text = monitorxp(args.cpu,args.gpu,args.mem)
+        else:
+            print("You can only choose 0,1,2,3 as the type of GPU!")
+            break
+
+        if i:
+            try:
+                ustcmail(args.sn,args.pw,args.ra,text)
+            except smtplib.SMTPAuthenticationError:
+                pass
+            else:
+                pass
+            print("Found!\n")
+            print(text)
+        else:
+            time_stamp = datetime.datetime.now()
+            print(time_stamp.strftime('%Y.%m.%d-%H:%M:%S')+" Not Found Yet!")
+            time.sleep(3)
+
+if __name__ == "__main__":
+    main()
